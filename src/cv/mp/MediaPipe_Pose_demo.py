055cbccf8849787a4875c3c31dc9e9febbbea46d
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2023-08-03 18:36:13 +0800

    Turn Pose into a thread

--- src/cv/mp/MediaPipe_Pose_demo.py
+++ src/cv/mp/MediaPipe_Pose_demo.py
@@ -6,6 +6,7 @@ import numpy as np
 import imutils
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
+from src.cv.mp.pose import PoseDetection
 
 from settings import S
 
@@ -18,39 +19,39 @@ mp_pose = mp.solutions.pose
 # For webcam input:
 cap = WebcamVideoStream(src=S.VIDEO_SOURCE).start()
 fps = FPS().start()
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.grabbed:
-    fps.update()
+pose = PoseDetection().start()
+while cap.grabbed:
+  fps.update()
 
-    image = cap.read()
+  image = cap.read()
 
-    # image = cv2.resize(image, None, fx=0.2, fy=0.2) # Reduce the size
-    image = imutils.resize(image, width=400)
+  # image = cv2.resize(image, None, fx=0.2, fy=0.2) # Reduce the size
+  image = imutils.resize(image, width=400)
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
+  # To improve performance, optionally mark the image as not writeable to
+  # pass by reference.
+  image.flags.writeable = False
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  
+  pose.read_image(image)
+  results = pose.process()
 
-    # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
-        image,
-        results.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Pose',
-               image
-              #  cv2.flip(image, 1) # DONT Flip
-               )
+  # Draw the pose annotation on the image.
+  image.flags.writeable = True
+  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  mp_drawing.draw_landmarks(
+      image,
+      results.pose_landmarks,
+      mp_pose.POSE_CONNECTIONS,
+      landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+  # Flip the image horizontally for a selfie-view display.
+  cv2.imshow('MediaPipe Pose',
+              image
+            #  cv2.flip(image, 1) # DONT Flip
+              )
 
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+  if cv2.waitKey(5) & 0xFF == 27:
+    break
 
 # stop the timer and display FPS information
 fps.stop()
--- src/cv/mp/Pose.py
+++ src/cv/mp/Pose.py
@@ -1,8 +1,67 @@
+# import the necessary packages
+from threading import Thread
 import numpy as np
 import cv2
 import mediapipe as mp
+
+
+import imutils
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+
+
+
+class PoseDetection:
+
+    def __init__(self, name="WebcamVideoStream"):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+
+        self.pose = mp_pose.Pose(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5)
+
+        # initialize the thread name
+        self.name = name
+
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+        
+        
+        H, W = 1, 1
+        self.image = np.ndarray((H, W, 3), np.uint8) # empty image
+        self.result = self.pose.process(self.image)
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        t = Thread(target=self.update, name=self.name, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            self.result = self.pose.process(self.image)
+
+    def read_image(self, image):
+        self.image = image
+
+    def process(self):
+        # return the frame most recently read
+        return self.result
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
