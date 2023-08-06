fc0e21b54496e6777701d47bdbec586d34e43b3e
penny <penny2014928@gmail.com>
2023-08-06 08:52:56 +0800

    can show top & bottom on the web

--- src/cv/app.py
+++ src/cv/app.py
@@ -1,5 +1,7 @@
 # 导入必要的库
 import cv2
+import csv
+# from cv import app
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, emit
@@ -55,7 +57,8 @@ def recognize_color_once(frame):
     # 返回颜色名称
     return color_result  # 示例颜色
 
-# 创建数据库模型
+
+    
 class Clothing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     colo_namer = db.Column(db.String(50), nullable=False)
@@ -63,22 +66,66 @@ class Clothing(db.Model):
     slot_location = db.Column(db.String(50))
     season = db.Column(db.String(50))
     clothing_type = db.Column(db.String(50))
+    memo = db.Column(db.String(50))
+
 
     def __repr__(self):
         return f"<Clothing {self.color}>"
+    
+# 创建新数据库模型
+class Clothing2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    colo_namer = db.Column(db.String(50), nullable=False)
+    photo_path = db.Column(db.String(200))
+    slot_location = db.Column(db.String(50))
+    season = db.Column(db.String(50))
+    clothing_type = db.Column(db.String(50))
+    memo = db.Column(db.String(50))
 
+
+    def __repr__(self):
+        return f"<Clothing {self.color}>"
+    
+# 在網頁載入資料庫資料
 @socketio.on('query_clothing', namespace='/')
 def query_clothing():
-    clothing = Clothing.query.all()
-    clothing_data = [{'id': c.id, 'colo_namer': c.colo_namer, 'photo_path': c.photo_path,
+    clothing2 = Clothing2.query.all()
+    clothing_data2 = [{'id': c.id, 'colo_namer': c.colo_namer, 'photo_path': c.photo_path,
                       'slot_location': c.slot_location, 'season': c.season, 'clothing_type': c.clothing_type}
-                     for c in clothing]
-    emit('clothing_data', clothing_data)
+                     for c in clothing2]
+    emit('clothing_data2', clothing_data2)
+
+# ...（之前的程式碼）
 
 # 主要逻辑
 if __name__ == '__main__':
     with app.app_context():
+        # 在應用程式上下文中建立資料庫
         db.create_all()
+
+        # # 从CSV文件读取数据并写入数据库
+        # csv_file_path = 'tests/all_clothes.csv'
+        # with open(csv_file_path, 'r', newline='') as csvfile:
+        #     csv_reader = csv.reader(csvfile)
+        #     for row in csv_reader:
+        #         id, photo_path, _, colo_namer, season, clothing_type, extra_field = row
+        #         clothing = Clothing2(
+        #             id=id,
+        #             colo_namer=colo_namer,
+        #             photo_path=photo_path,
+        #             slot_location=_,  # 此字段在CSV中没有提供，将其设置为占位符"_"，您可以根据需求进行调整
+        #             season=season,
+        #             clothing_type=clothing_type,
+        #             memo=extra_field
+        #         )
+        #         db.session.add(clothing)
+
+        #     db.session.commit()
+
+    # 在 Flask 應用程式內執行資料庫相關操作
+    with app.app_context():
+        app.run()  # 启动Flask应用
+
 
     # cap = cv2.VideoCapture(0)  # 打开摄像头
     # while True:
@@ -100,4 +147,4 @@ if __name__ == '__main__':
     # cap.release()
     # cv2.destroyAllWindows()
 
-    app.run()  # 启动Flask应用
+    # app.run()  # 启动Flask应用
