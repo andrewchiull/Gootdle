55c820d42bf3a7671fd045063547b032a6023f93
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2023-08-23 17:28:09 +0800

    Create logger

--- sandbox/import_module/module/module.py
+++
@@ -1,2 +0,0 @@
-from sandbox.import_module.settings import S
-print(S.ROOT_DIR)
--- sandbox/import_module/settings.py
+++ sandbox/import_module/settings.py
@@ -12,5 +12,30 @@ class Settings(BaseSettings):
 S = Settings()
 
 
+def create_logger(NAME: str):
+    import logging
+    import sys
 
+    LEVEL = logging.INFO
+    # NAME = Path(current_file).name
+    FILE = S.ROOT_DIR / "logs" / (NAME + ".log")
 
+    # TODO try logging.config
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+    logger = logging.getLogger(NAME)
+    logger.setLevel(LEVEL)
+
+    file_handler = logging.FileHandler(filename=FILE, mode='a')
+    file_handler.setLevel(LEVEL)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(LEVEL)
+    console_handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+    return logger
